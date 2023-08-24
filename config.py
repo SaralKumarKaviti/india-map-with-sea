31785857e68ca023b8eaeb9a6848655dee52a440
SaralKumarKaviti <saralkumar238@gmail.com>
2023-08-24 15:21:51 +0530

    code in connect venv

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
-# from mongoengine import connect
-# # import pymongo
-# # from pymongo import MongoClient
-
-# # client = connect('studentprofiles',host='localhost',port=27017)
-
-# connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles"
-# client=connect(host=connection_string)
-
-from retrying import retry
 from mongoengine import connect
-import pymongo.errors
+# import pymongo
+# from pymongo import MongoClient
 
-@retry(wait_fixed=2000, stop_max_attempt_number=5)
-def connect_to_mongodb():
-    try:
-        connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles?ssl=true"
-        client = connect(host=connection_string)
-        return client
-    except pymongo.errors.ServerSelectionTimeoutError as e:
-        print("Connection failed. Retrying...")
-        raise e
+# client = connect('studentprofiles',host='localhost',port=27017)
 
-db_client = connect_to_mongodb()
+connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles?ssl=true"
+client=connect(host=connection_string)
+
+# from retrying import retry
+# from mongoengine import connect
+# import pymongo.errors
+
+# @retry(wait_fixed=2000, stop_max_attempt_number=5)
+# def connect_to_mongodb():
+#     try:
+#         connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles?ssl=true"
+#         client = connect(host=connection_string)
+#         return client
+#     except pymongo.errors.ServerSelectionTimeoutError as e:
+#         print("Connection failed. Retrying...")
+#         raise e
+
+# db_client = connect_to_mongodb()
