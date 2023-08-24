b637faeaf6b03dfb4ecf406180bd664e2d691f5d
SaralKumarKaviti <saralkumar238@gmail.com>
2023-08-24 15:29:10 +0530

    doing changes to connect db

--- app.py
+++ app.py
@@ -769,4 +769,5 @@ def alogout():
     return redirect(url_for('adminIndex'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    # app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(debug=True)
