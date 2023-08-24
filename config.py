f1bf53ff812ba8b420269a14943ca46217c8406e
SaralKumarKaviti <saralkumar238@gmail.com>
2023-08-24 14:40:48 +0530

    code in connect venv

--- config.py
+++ config.py
@@ -1,8 +1,24 @@
+# from mongoengine import connect
+# # import pymongo
+# # from pymongo import MongoClient
+
+# # client = connect('studentprofiles',host='localhost',port=27017)
+
+# connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles"
+# client=connect(host=connection_string)
+
+from retrying import retry
 from mongoengine import connect
-# import pymongo
-# from pymongo import MongoClient
+import pymongo.errors
 
-# client = connect('studentprofiles',host='localhost',port=27017)
+@retry(wait_fixed=2000, stop_max_attempt_number=5)
+def connect_to_mongodb():
+    try:
+        connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles"
+        client = connect(host=connection_string)
+        return client
+    except pymongo.errors.ServerSelectionTimeoutError as e:
+        print("Connection failed. Retrying...")
+        raise e
 
-connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles"
-client=connect(host=connection_string)
+db_client = connect_to_mongodb()
