77efafa0af80da27dd7d31038ceb3bb28dfbb992
SaralKumarKaviti <saralkumar238@gmail.com>
2023-08-26 11:40:00 +0530

    changes in database connection

--- config.py
+++ config.py
@@ -22,3 +22,24 @@ client=connect(host=connection_string)
 #         raise e
 
 # db_client = connect_to_mongodb()
+
+from pymongo import MongoClient
+import ssl
+
+# Replace this with your actual connection string
+connection_string = "mongodb+srv://saralkumar001:PoUhiEHIBBP970Xf@cluster0.pv0sybl.mongodb.net/studentprofiles"
+
+# Create a MongoClient instance using the connection string
+client = MongoClient(connection_string, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
+
+# Access the "studentprofiles" database
+db = client.studentprofiles
+
+# Now you can work with the database collections
+# # For example:
+# collection = db.students
+# result = collection.find_one({"name": "John Doe"})
+# print(result)
+
+# Remember to close the connection when you're done
+client.close()
